46d0ea3e54f3f6c63d85504d03c8c3440508fffd
ashoka-shringla <ashoka-shringla@users.noreply.github.com>
2025-03-06 19:03:26 +0000

    Auto-save from cloud IDE on 2025-03-06 19:03:26

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,7 @@ public class Lab {
      * @return "Hello, world!"
      */
     public String sayHello(){
+        System.out.println("ec2 testing with the cloud services table!")
         return "change me";das
     }
 }
